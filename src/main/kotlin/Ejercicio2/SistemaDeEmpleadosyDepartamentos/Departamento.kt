46d44bbb100ca790cd11811f230c373f9bcba27a
javierac11 <javieracedocastrillon@gmail.com>
2024-02-26 10:31:09 +0100

    Class Departamento terminada

---
+++ src/main/kotlin/Ejercicio2/SistemaDeEmpleadosyDepartamentos/Departamento.kt
@@ -0,0 +1,17 @@
+package org.example.Ejercicio2.SistemaDeEmpleadosyDepartamentos
+
+class Departamento(private var listaEmpleados: MutableList<Empleado> = mutableListOf()) {
+
+    fun addEmpleado(empleado: Empleado){
+        listaEmpleados.add(empleado)
+    }
+
+    fun calcularSalarioTotal(): Double{
+        var salario = 0.0
+        listaEmpleados.forEach(){
+            salario += it.calcularSalario()
+        }
+        return salario
+    }
+
+}
--- src/main/kotlin/Ejercicio2/SistemaDeEmpleadosyDepartamentos/Empleado.kt
+++ src/main/kotlin/Ejercicio2/SistemaDeEmpleadosyDepartamentos/Empleado.kt
@@ -1,11 +1,11 @@
 package org.example.Ejercicio2.SistemaDeEmpleadosyDepartamentos
 
 /**
- * Considera añadir una clase Departamento que tenga una lista de empleados y pueda calcular el salario total que se debe pagar a todos sus empleados.
  *
- * Departamento podría tener una lista de empleados y dos métodos: agregarEmpleado y calculaSalarioTotal que tienen sus empleados al mes.
- *
- * En el main crea una instancia de Departamento, agrega varios empleados, recorre la lista de los empleados mostrando su información "Nombre con ID-0001 tiene un salario de 28697.96 al mes." (el id siempre con 4 posiciones numéricas y el salario con 2 decimales)
+ * En el main crea una instancia de Departamento, agrega varios empleados, recorre la lista de los empleados mostrando
+ * su información
+ * "Nombre con ID-0001 tiene un salario de 28697.96 al mes."
+ * (el id siempre con 4 posiciones numéricas y el salario con 2 decimales)
  *
  * ¿Se te ocurre alguna restricción lógica que podríamos introducir a las propiedades?
  *
